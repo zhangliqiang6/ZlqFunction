77125dc0baa6ce7b8a18736023dffcf9c428a6e2
zhangliqiang6 <43942359+zhangliqiang6@users.noreply.github.com>
2023-07-21 23:24:31 +0800

    新建项目

---
+++ Source/ZlqFunction/ZlqFunction.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZlqFunction.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZlqFunction, "ZlqFunction" );
---
+++ Source/ZlqFunction/ZlqFunctionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZlqFunctionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZLQFUNCTION_API AZlqFunctionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
